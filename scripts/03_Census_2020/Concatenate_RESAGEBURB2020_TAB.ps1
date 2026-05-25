bf881b1a7fac1ab9809e7348462a09481d4f0a4c
Carlos Blanco <mapanet.es@gmail.com>
2026-05-24 20:49:24 -0500

    Update Concatenate_RESAGEBURB2020_TAB.ps1

--- scripts/03_Census_2020/Concatenate_RESAGEBURB2020_TAB.ps1
+++ scripts/03_Census_2020/Concatenate_RESAGEBURB2020_TAB.ps1
@@ -2,7 +2,7 @@
 Set-Location -Path (Split-Path -Parent $MyInvocation.MyCommand.Definition)
 
 # Path where the 32 "RESAGEBURB2020 - **NN** Name .csv" files are located
-$inputFolder = "D:\Postal Codes Databases\Mexico MX\INEGI.org.mx\Censos 2020\Tabulados AGEB Manzana"
+$inputFolder = "D:\INEGI\Censo_2020"
 
 # Final combined output file CSV (TSV)
 $outputFile = Join-Path $inputFolder "RESAGEBURB2020_ALL_TAB.csv"
